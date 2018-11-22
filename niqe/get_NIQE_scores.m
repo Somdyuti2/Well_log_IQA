d9de54641c1772440ee61d9a4e7382c9949ab34a
Somdyuti <somdyuti@utexas.edu>
2018-11-22 16:34:53 -0600

    Added dataset B

--- niqe/get_NIQE_scores.m
+++ niqe/get_NIQE_scores.m
@@ -8,45 +8,16 @@ blocksizerow    = 96;
 blocksizecol    = 96;
 blockrowoverlap = 0;
 blockcoloverlap = 0;
-
-current = pwd;
+arg=input('Enter ''A'' to evaluate on dataset A and ''B'' to evaluate on dataset B:','s');
 
 % Compute NIQE Scores of test well logs from 'acceptable' category
-folderpath   = '..\Test\Acceptable';
-cd(sprintf('%s',folderpath))
-names        = ls;
+folderpath   = strcat('..\Test\dataset_', arg, '\Acceptable');
+names        = dir(folderpath);
 names        = names(3:end,:);
-cd(current)
-sum = 0, count = 0;
-for itr = 1:length(names)
-    itr    
-sprintf('%s\\%s',folderpath,names(itr,:));  
-im               = imread(sprintf('%s\\%s',folderpath,names(itr,:)));
-if var(double(im(:)))==0
-    continue
-end
-[row col] = size(im);
-if(row>=blocksizerow && col>=blocksizecol)
-    quality = computequality(im,blocksizerow,blocksizecol,blockrowoverlap,blockcoloverlap, ...
-    mu_prisparam,cov_prisparam);
-    sum = sum + quality;
-    Q(itr) = quality
-    count = count + 1;
-end
-end
-save quality_acceptable Q
-
-%% Compute NIQE Scores of test well logs from 'unacceptable' category
-folderpath   = '..\Test\Unacceptable';
-cd(sprintf('%s',folderpath))
-names        = ls;
-names        = names(3:end,:);
-cd(current)
-sum = 0, count = 0;
-for itr = 1:length(names)
-    itr    
-sprintf('%s\\%s',folderpath,names(itr,:));  
-im               = imread(sprintf('%s\\%s',folderpath,names(itr,:)));
+sum = 0;
+count = 0;
+for itr = 1:length(names)  
+im               = imread(sprintf('%s\\%s',folderpath,names(itr).name));
 if var(double(im(:)))==0
     continue
 end
@@ -56,6 +27,27 @@ if(row>=blocksizerow && col>=blocksizecol)
     mu_prisparam,cov_prisparam);
     sum = sum + quality;
     Q(itr) = quality;
+    count = count + 1;
+end
+end
+save quality_acceptable Q
+
+%% Compute NIQE Scores of test well logs from 'unacceptable' category
+folderpath   = strcat('..\Test\dataset_', arg, '\Unacceptable');
+names        = dir(folderpath);
+names        = names(3:end,:);
+sum = 0, count = 0;
+for itr = 1:length(names)  
+im               = imread(sprintf('%s\\%s',folderpath,names(itr).name));
+if var(double(im(:)))==0
+    continue
+end
+[row col] = size(im);
+if(row>=blocksizerow && col>=blocksizecol)
+    quality = computequality(im,blocksizerow,blocksizecol,blockrowoverlap,blockcoloverlap, ...
+    mu_prisparam,cov_prisparam);
+    sum = sum + quality;
+    Q(itr) = quality
     count = count + 1;
 end
 end
